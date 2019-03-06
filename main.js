a0b839325abfc10e7455f6df2a636e3dd0963623
Silvestre Luna <slvtrluna@gmail.com>
2019-03-05 20:25:06 -0600

    adding 3 files

--- main.js
+++ main.js
@@ -1,1 +1,42 @@
-console.log('hello!')
+const dinosaurs = [
+    {
+        dinoType: 't-rex',
+        name: 'wrex',
+    },
+    {
+        dinoType: 'stegosaurus',
+        name: 'steve',
+    },
+    {
+        dinoType: 'velocirapto',
+        name: 'ted',
+    }
+];
+
+//loop over the dinosaus
+// built up a domstring
+// print domstring to the dom 
+const printToDom = (divId, textToPrint) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML = textToPrint;
+};
+
+const buildDinosaurs = () => {
+    let domString = '';
+    for (let i = 0; i <  dinosaurs.length; i++) {
+        domString += `<div class="dinosaur">`
+        domString += `<h3>${dinosaurs[i].dinoType}</h3>`;
+        domString += `<p>${dinosaurs[i].name}</p>`;
+        domString += `</div>`;
+    }
+    printToDom('dino-barn', domString);
+};
+
+
+
+
+const init = () => {
+  buildDinosaurs();
+};
+
+init();
